91ca3ae78af817c3ba2f8e500018ddd4f49baea4
Tannky93 <collinge1@msn.com>
2023-04-17 04:49:43 +0100

    timer code update

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const tmi = require('tmi.js');
 const fs = require('fs');
 
 const regexpCommand = new RegExp(/^!([a-zA-z0-9]+)(?:\w+)?(.*)?/);
+const digExtract = new RegExp(/^\D+/);
 
 const commands = {
     website: {
@@ -17,6 +18,9 @@ const commands = {
     },
     botupdate:{
         response: 'botupdate'
+    },
+    timer:{
+        response: 'timer'
     }
 }
 
@@ -61,6 +65,9 @@ client.on('message', (channel, tags, message, self) => {
                     break;
                 case 'botupdate':
                     ReadFromFile();
+                    break;
+                case 'timer':
+                    Timer(channel,message);
                     break;
                 default:
                     client.say(channel,response);
@@ -148,8 +155,10 @@ function ReadFromFile(){
 
 }
 
-async function Timer(channel, message, length) {
-    var length = await TimerParseTime(message);
+async function Timer(channel, message) {
+    var length = TimerParseTime(message);
+    client.say(channel,`The length of the timer is ${length} seconds`);
+
 
     
 }
@@ -160,19 +169,41 @@ function TimerParseTime(message){
     // 4:20:59:59 = 4 days 20 hours 59 mins 59 seconds.
     // single length = seconds, 2 length = mins + seconds, 
     // 3 length = hours + mins + seconds, 4 length = days + hours + mins + seconds
-    var parsedmessage = message.split(":");
-    
-    switch(parsedmessage.length){
+    console.log("Message: " + message);
+    var digitExtract = message.replace(digExtract,'');
+    console.log("Extract: " + digitExtract);
+    var parsedMessage = digitExtract.split(":");
+    console.log("first value: " + parsedMessage[0]);
+    console.log("Length: " + parsedMessage.length);
+
+    switch(parsedMessage.length){
         case 1:
+            console.log("case 1");
+            length = parseInt(parsedMessage[0]);
+            console.log(length + " Seconds")
             break;
         case 2:
+            console.log("case 2");
+            console.log (parsedMessage[0]+" minutes and " + parsedMessage[1] + " seconds");
+            length = (parseInt(parsedMessage[0]) * 60) + parseInt(parsedMessage[1]);
+            console.log(length + " seconds");
             break;
         case 3:
+            console.log("case 3");
+            console.log (parsedMessage[0]+" hours and "+parsedMessage[1]+" minutes and " + parsedMessage[2] + " seconds");
+            length = ((parseInt(parsedMessage[0]) * 60) * 60) + (parseInt(parsedMessage[1]) * 60) + parseInt(parsedMessage[2]);
+            console.log(length + " seconds");
             break;
         case 4:
+            console.log("case 4");
+            console.log (parsedMessage[0]+" days and "+ parsedMessage[1]+" hours and " + parsedMessage[2] + " minutes and " + parsedMessage[3] + " seconds");
+            length = (((parseInt(parsedMessage[0]) * 24) * 60) * 60) + ((parseInt(parsedMessage[1]) * 60) * 60) + (parseInt(parsedMessage[2]) * 60) + parseInt(parsedMessage[3]);
+            console.log(length + " seconds");
+            
             break;
         default:
             length = 1;
+            console.log("case Default")
             break;
     }
 
